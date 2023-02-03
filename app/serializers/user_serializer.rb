38488755e6dbcc3ce2ef87a602db5b403fb71022
Zachary Sirna <zsirna@yahoo.com>
2023-02-03 15:09:55 -0500

    building active storage for photos on mydogs

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,9 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :first_name, :state, :country, :email
 
   has_many :dogs
+
+  # def dog_image
+  #   byebug
+  # end
+
 end
--- client/src/MyDog.js
+++ client/src/MyDog.js
@@ -1,33 +1,37 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "./Context";
+import "./DogImage.css"
 
 const MyDog = () => {
-  const { user } = useContext(UserContext);
-  const [dogPhoto, setDogPhoto] = useState([])
+  const { user, dogs } = useContext(UserContext);
+  const [dogImage, setDogImage] = useState([]);
 
-  useEffect (() => {
-    fetch('/images')
-    .then(res => res.json())
-    .then(data => setDogPhoto(data))
-  })
+console.log("in dog", dogs)
 
-  const handleSubmitPhoto = () => {
-    e.preventDefault()
-    // you need to add a dog_id column to images
+  const handleSubmitPhoto = (e) => {
+    e.preventDefault();
 
-  }
+    const formData = new FormData();
+    formData.append("dog_id", e.target.id);
+    formData.append("dog_image", dogImage);
 
+    fetch("/images", {
+      method: "POST",
+      body: formData,
+    });
+  };
 
-  return  (
+  return (
     <div>
       <br></br>
       <h1>{user.first_name}</h1>
       <h1>{user.username}'s Dogs</h1>
+      {/* <img src={dogs[0].image.dog_image} alt="my dog" /> */}
       <div>
         {user.dogs.map((dog) => {
-        return(
-          <div key={dog.id}>
-            <h2>{dog.name}</h2>
+          return (
+            <div key={dog.id}>
+              <h2>{dog.name}</h2>
               <ul>
                 <li>{dog.breed}</li>
                 <li>{dog.traits}</li>
@@ -35,12 +39,22 @@ const MyDog = () => {
                 <li>{dog.age}</li>
                 <li>{dog.vaccination ? "Yes" : "Not Yet"}</li>
               </ul>
-          </div>
-        )
+              <h1>Add Photo</h1>
+              <form onSubmit={handleSubmitPhoto} id={dog.id}>
+                <h3>upload photo</h3>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => setDogImage(e.target.files[0])}
+                />
+                <input type="submit" />
+              </form>
+            </div>
+          );
         })}
       </div>
     </div>
-  )
+  );
 };
 
 export default MyDog;
