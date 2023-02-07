c23a4443a645b60ed8082b14225f33c42bb16b19
Zachary Sirna <zsirna@yahoo.com>
2023-02-07 13:02:57 -0500

    building home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Welcome to Friendo</h1>
+      <h3>We aim to find your dog or dogs their new dog best friend.</h3>
+      <p>
+        After two years of working from home not only have we spent too much
+        time apart but so have our dogs.
+        <br></br>Anti-social dogs can not only present problems inside the house
+        but they are also more likely to be aggressive outside the house.
+      </p>
+      <h2>Issues related to anti-social behavior</h2>
+      <h3>Inside the home:</h3>
+      <ul>
+        <li>Avoids other dogs or people</li>
+        <li>Pulls or lunges on walks</li>
+        <li>Not respecting personal space</li>
+      </ul>
+      <h3>Outside the home:</h3>
+      <ul>
+        <li>Barking</li>
+        <li>Growling</li>
+        <li>Baring Teeth</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
