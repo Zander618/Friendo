14849a120f9c9e19018b9ee00b9f7ece04cf5cc6
Zachary Sirna <zsirna@yahoo.com>
2023-03-02 14:07:04 -0500

    before removing dog meetups table in order to deploy

--- app/models/dog_meetup.rb
+++
@@ -1,5 +0,0 @@
-# class DogMeetup < ApplicationRecord
-#   belongs_to :invitor, class_name: 'Dog'
-#   belongs_to :invitee, class_name: 'Dog'
-#   belongs_to :meetup
-# end
--- app/serializers/dogmeetup_serializer.rb
+++
@@ -1,8 +0,0 @@
-# class DogmeetupSerializer < ActiveModel::Serializer
-#   attributes :id, :meetup_id, :invitee_id, :invitor_id
-
-#   belongs_to :invitor, class_name: 'Dog'
-#   belongs_to :invitee, class_name: 'Dog'
-#   belongs_to :meetup
-
-# end
