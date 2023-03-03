bf77c89dd71518a00dc44367201063c6d2892d5f
Zachary Sirna <zsirna@yahoo.com>
2023-03-03 15:20:12 -0500

    adding fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
+  namespace :api do
 
-  get "/me", to: "users#show"
+    get "/me", to: "users#show"
 
-  resources :locations
-  resources :meetups
-  # resources :dog_meetups
-  resources :dogs
-  resources :users
-  resources :images
+    resources :locations
+    resources :meetups
+    resources :dogs
+    resources :users
+    resources :images
 
-  post "/signup", to: "users#create"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+    post "/signup", to: "users#create"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
