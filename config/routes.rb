c4644b53d672708016d1507a46166ed9c9df1aad
Zachary Sirna <zsirna@yahoo.com>
2023-03-07 14:48:29 -0500

    moving fallback controller back to the top

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
 
   get "/me", to: "users#show"
 
@@ -13,9 +15,5 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-
-  get '*path',
-  to: 'fallback#index',
-  constraints: ->(req) { !req.xhr? && req.format.html? }
 
 end
