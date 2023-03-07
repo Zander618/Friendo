a226398c218e899f9e6d364509a4d8ecc9654b08
Zachary Sirna <zsirna@yahoo.com>
2023-03-07 13:48:13 -0500

    uncommenting fallback controller and moving it to the top of the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
-  # namespace :api do
 
-    get "/me", to: "users#show"
+  get '*path',
+  to: 'fallback#index',
+  constraints: ->(req) { !req.xhr? && req.format.html? }
 
-    resources :locations
-    resources :meetups
-    resources :dogs
-    resources :users
-    resources :images
+  get "/me", to: "users#show"
 
-    post "/signup", to: "users#create"
-    post "/login", to: "sessions#create"
-    delete "/logout", to: "sessions#destroy"
+  resources :locations
+  resources :meetups
+  resources :dogs
+  resources :users
+  resources :images
 
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
- 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
