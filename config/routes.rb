20e8ed3c297ac5e419441be787f452d2e369621e
Zachary Sirna <zsirna@yahoo.com>
2023-03-13 19:55:54 -0400

    fallback controller accidently turned off

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  # get '*path',
-  #   to: 'fallback#index',
-  #   constraints: ->(req) { !req.xhr? && req.format.html? }
+  get '*path',
+    to: 'fallback#index',
+    constraints: ->(req) { !req.xhr? && req.format.html? }
 
   get "/me", to: "users#show"
 
