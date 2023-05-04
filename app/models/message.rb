b5ac1441f2b728245662e4e03fea2c4f7b88ca96
Zachary Sirna <zsirna@yahoo.com>
2023-05-04 19:52:47 -0400

    uncommenting out model and serializer

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
-# class Message < ApplicationRecord
-#   belongs_to :user
+class Message < ApplicationRecord
+  belongs_to :user
 
-#   after_create_commit { broadcast_append_to current_user }
-# end
+  after_create_commit { broadcast_append_to current_user }
+end
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,3 @@
-# class MessageSerializer < ActiveModel::Serializer
-#   attributes :id, :invitor_id, :invitee_id, :meetup_id, :text, :date, :time
-# end
+class MessageSerializer < ActiveModel::Serializer
+  attributes :id, :invitor_id, :invitee_id, :meetup_id, :text, :date, :time
+end
