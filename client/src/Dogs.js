3951f7b3e9d504a55732b76a8b61fbb32dbfdd6c
Zachary Sirna <zsirna@yahoo.com>
2023-05-19 15:26:12 -0400

    working on css layout for dogs

--- client/src/Dogs.js
+++ client/src/Dogs.js
@@ -1,11 +1,11 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import { UserContext } from "./Context";
-import "./DogImage.css"
+import "./DogImage.css";
 import { useNavigate } from "react-router-dom";
 
-const Dogs = ( { setDogId } ) => {
+const Dogs = ({ setDogId }) => {
   const navigate = useNavigate();
-  
+
   const { dogs, userId } = useContext(UserContext);
 
   // const handleClick = (e) => {
@@ -13,38 +13,55 @@ const Dogs = ( { setDogId } ) => {
   //   navigate(`/dogs/:id/meetups/new`)
   // }
 
-  let filteredDogs = dogs.filter((dog) => dog.user_id !== userId)
-
+  let filteredDogs = dogs.filter((dog) => dog.user_id !== userId);
 
   return dogs ? (
     <div>
-      {filteredDogs.sort((a, b) => (a.date > b.date ? 1 : -1)).map((dog) => {
-        return(
-        <div key={dog.id} className="dog-card">
-          <h1>{dog.name}</h1>
-          <h2>{dog.owner_username}'s dog</h2>
-          <img src={dog.uploaded_image ? dog.uploaded_image : "no photo"} alt="Dog not added" className="dogImageSizing"/>
-            <ol>
-                <p>Breed: {dog.breed}</p>
-                <p>Personality Traits: {dog.traits}</p>
-                <p>Enjoyed Activites: {dog.enjoyed_activities}</p>
-                <p>Age: {dog.age}</p>
-                <p>Vaccination Status: {dog.vaccination ? "Yes" : "Not Yet"}</p>
-                <p>Has sent {dog.sent_invitations.length} invitations</p>
-                <p>Has received {dog.recieved_invitations.length} invitations</p>
-                <button onClick={() => {
-                  setDogId(dog.id)
-                  navigate(`/dogs/${dog.id}/meetups/new`)
-                  }} id={dog.id}>Request a Meetup</button>
-            </ol>
-        </div>
-        )
-      })}
+      {filteredDogs
+        .sort((a, b) => (a.date > b.date ? 1 : -1))
+        .map((dog) => {
+          return (
+            <div key={dog.id} className="dog-card">
+              <div>
+                <h1>{dog.name}</h1>
+                <h2>{dog.owner_username}'s dog</h2>
+                  <img
+                    src={dog.uploaded_image ? dog.uploaded_image : "no photo"}
+                    alt="Dog not added"
+                    className="dogImageSizing"
+                  />
+                <div className="side-content-center">
+                  <ol>
+                    <p>Breed: {dog.breed}</p>
+                    <p>Personality Traits: {dog.traits}</p>
+                    <p>Enjoyed Activites: {dog.enjoyed_activities}</p>
+                    <p>Age: {dog.age}</p>
+                    <p>
+                      Vaccination Status: {dog.vaccination ? "Yes" : "Not Yet"}
+                    </p>
+                    <p>Has sent {dog.sent_invitations.length} invitations</p>
+                    <p>
+                      Has received {dog.recieved_invitations.length} invitations
+                    </p>
+                    <button
+                      onClick={() => {
+                        setDogId(dog.id);
+                        navigate(`/dogs/${dog.id}/meetups/new`);
+                      }}
+                      id={dog.id}
+                    >
+                      Request a Meetup
+                    </button>
+                  </ol>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
-    
   ) : (
     <h1>...Loading</h1>
-  )
-}
+  );
+};
 
-export default Dogs
+export default Dogs;
