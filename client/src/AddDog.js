ebea355d26506f1f074bcea077e372b4a3764828
Zachary Sirna <zsirna@yahoo.com>
2023-05-31 13:54:59 -0400

    creating dog breed list

--- client/src/AddDog.js
+++ client/src/AddDog.js
@@ -6,12 +6,67 @@ import Select from "react-select";
 const AddDog = () => {
   const { user, setUser, userId, dogs, setDogs, setUserDogs } = useContext(UserContext);
   const [selectedVaccinationStatus, setSelectedVaccinationStatus] = useState("");
+  const [selectedBreed, setSelectedBreed] = useState("");
   const [errors, setErrors] = useState([]);
   const navigate = useNavigate();
 
-  const options = [
+  const vaccinationOptions = [
     { value: 1, label: "Vaccinated" },
     { value: 0, label: "Not Vaccinated Yet" },
+  ];
+
+  const breedOptions = [
+    { value: 0, label: "Airedale Terrier" },
+    { value: 1, label: "Akita" },
+    { value: 2, label: "Alaskan Malamute" },
+    { value: 3, label: "American Staffordshire Terrier" },
+    { value: 4, label: "Anatolian Shepherd Dog" },
+    { value: 5, label: "Australian Cattle Dog" },
+    { value: 6, label: "Australian Shepherd" },
+    { value: 7, label: "Basenji" },
+    { value: 8, label: "Basset Hound" },
+    { value: 9, label: "Beagle" },
+    { value: 10, label: "Belgian Malinoi" },
+    { value: 11, label: "Bernese Mountain Dog" },
+    { value: 12, label: "Bichons Frise" },
+    { value: 13, label: "Biewer Terrier" },
+    { value: 14, label: "Bloodhound" },
+    { value: 15, label: "Border Collie" },
+    { value: 16, label: "Border Terrier" },
+    { value: 17, label: "Boston Terrier" },
+    { value: 18, label: "Bouviers des Flandre" },
+    { value: 19, label: "Boxer" },
+    { value: 20, label: "Boykin Spaniel" },
+    { value: 21, label: "Brittany" },
+    { value: 22, label: "Brussels Griffon" },
+    { value: 23, label: "Bull Terrier" },
+    { value: 24, label: "Bulldog" },
+    { value: 25, label: "Bullmastiff" },
+    { value: 26, label: "Vaccinated" },
+    { value: 27, label: "Not Vaccinated Yet" },
+    { value: 28, label: "Vaccinated" },
+    { value: 29, label: "Not Vaccinated Yet" },
+    { value: 30, label: "Vaccinated" },
+    { value: 31, label: "Not Vaccinated Yet" },
+    { value: 32, label: "Vaccinated" },
+    { value: 33, label: "Not Vaccinated Yet" },
+    { value: 34, label: "Vaccinated" },
+    { value: 35, label: "Not Vaccinated Yet" },
+    { value: 36, label: "Vaccinated" },
+    { value: 37, label: "Not Vaccinated Yet" },
+    { value: 38, label: "Vaccinated" },
+    { value: 39, label: "Not Vaccinated Yet" },
+    { value: 40, label: "Vaccinated" },
+    { value: 41, label: "Not Vaccinated Yet" },
+    { value: 42, label: "Vaccinated" },
+    { value: 43, label: "Not Vaccinated Yet" },
+    { value: 44, label: "Vaccinated" },
+    { value: 45, label: "Not Vaccinated Yet" },
+    { value: 46, label: "Vaccinated" },
+    { value: 47, label: "Not Vaccinated Yet" },
+    { value: 48, label: "Vaccinated" },
+    { value: 49, label: "Not Vaccinated Yet" },
+
   ];
 
   const addNewDogToUser = (dog) => {
@@ -53,7 +108,7 @@ const AddDog = () => {
       body: JSON.stringify({
         user_id: userId,
         name: formData.name,
-        breed: formData.breed,
+        breed: selectedBreed,
         traits: formData.traits,
         enjoyed_activities: formData.enjoyed_activities,
         age: formData.age,
@@ -95,6 +150,10 @@ const AddDog = () => {
     setSelectedVaccinationStatus(selectedOption.value);
   };
 
+  const handleSelectedBreed = (selectedOption) => {
+    setSelectedBreed(selectedOption.value);
+  };
+
   return (
     <div>
       <h1>Add Dog</h1>
@@ -113,14 +172,7 @@ const AddDog = () => {
         <br></br>
         <label style={{ color: "black" }}>
           Breed:
-          <input
-            type="text"
-            name="breed"
-            spellCheck="true"
-            placeholder="Enter Dog's Breed"
-            value={formData.breed}
-            onChange={handleChange}
-          />
+          <Select options={breedOptions} onChange={handleSelectedBreed} autoFocus={true} />
         </label>
         <br></br>
         <label style={{ color: "black" }}>
@@ -161,7 +213,7 @@ const AddDog = () => {
         <br></br>
         <label style={{ color: "black" }}>
           Vaccination:
-          <Select options={options} onChange={handleSelectedVaccination} autoFocus={true} />
+          <Select options={vaccinationOptions} onChange={handleSelectedVaccination} autoFocus={true} />
         </label>
         <input type="submit" value="Submit" />
       </form>
