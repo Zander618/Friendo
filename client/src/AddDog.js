05304e40230352a1e9eee0466ffc6e6e30efaf28
Zachary Sirna <zsirna@yahoo.com>
2023-06-01 14:35:10 -0400

    installing new gems for new select feature

--- client/src/AddDog.js
+++ client/src/AddDog.js
@@ -1,7 +1,21 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, MouseEventHandler } from "react";
 import { UserContext } from "./Context";
 import { useNavigate } from "react-router-dom";
-import Select from "react-select";
+import Select, {
+  components,
+  MultiValueGenericProps,
+  MultiValueProps,
+  OnChangeValue,
+  Props,
+} from 'react-select';
+import {
+  SortableContainer,
+  SortableContainerProps,
+  SortableElement,
+  SortEndHandler,
+  SortableHandle,
+} from 'react-sortable-hoc';
+import { ColourOption, colourOptions } from '../data';
 
 const AddDog = () => {
   const { user, setUser, userId, dogs, setDogs, setUserDogs } = useContext(UserContext);
@@ -203,6 +217,62 @@ const AddDog = () => {
   const handleSelectedBreed = (selectedOption) => {
     setSelectedBreed(selectedOption.value);
   };
+
+
+// Testing new select feature
+
+
+function arrayMove<T>(array: readonly T[], from: number, to: number) {
+  const slicedArray = array.slice();
+  slicedArray.splice(
+    to < 0 ? array.length + to : to,
+    0,
+    slicedArray.splice(from, 1)[0]
+  );
+  return slicedArray;
+}
+
+const SortableMultiValue = SortableElement(
+  (props: MultiValueProps<ColourOption>) => {
+    // this prevents the menu from being opened/closed when the user clicks
+    // on a value to begin dragging it. ideally, detecting a click (instead of
+    // a drag) would still focus the control and toggle the menu, but that
+    // requires some magic with refs that are out of scope for this example
+    const onMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+    const innerProps = { ...props.innerProps, onMouseDown };
+    return <components.MultiValue {...props} innerProps={innerProps} />;
+  }
+);
+
+const SortableMultiValueLabel = SortableHandle(
+  (props: MultiValueGenericProps) => <components.MultiValueLabel {...props} />
+);
+
+const SortableSelect = SortableContainer(Select) as React.ComponentClass<
+  Props<ColourOption, true> & SortableContainerProps
+>;
+
+export default function MultiSelectSort() {
+  const [selected, setSelected] = React.useState<readonly ColourOption[]>([
+    colourOptions[4],
+    colourOptions[5],
+  ]);
+
+  const onChange = (selectedOptions: OnChangeValue<ColourOption, true>) =>
+    setSelected(selectedOptions);
+
+  const onSortEnd: SortEndHandler = ({ oldIndex, newIndex }) => {
+    const newValue = arrayMove(selected, oldIndex, newIndex);
+    setSelected(newValue);
+    console.log(
+      'Values sorted:',
+      newValue.map((i) => i.value)
+    );
+  };
+
 
   return (
     <div>
