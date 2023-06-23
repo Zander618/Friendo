99ed1cbabdb9a44535f4419e3a4c10d8347ffb42
Zachary Sirna <zsirna@yahoo.com>
2023-06-23 16:43:25 -0400

    finished redesign for now

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,14 +6,12 @@ const Home = () => {
   return (
     <div>
       <div className="banner">
-        <br />
-        <h1 className="text">Welcome to Friendo</h1>
+        <h1 className="text">Welcome to Friendo!</h1>
         <h3 className="text">Find your best friend their new best friend.</h3>
-        <br/>
       </div>
       <div className="container">
         <div classname="side">
-          <img className="picture" src="SignUp.png" alt="Friendo App"/>
+          <img className="picture" src="SignUp.PNG" alt="Friendo App"/>
           <h3>Sign Up</h3>
           <p>Sign-up with the link below to add your information.</p>
           <Link
@@ -30,11 +28,11 @@ const Home = () => {
           </Link>
         </div>
         <div classname="side">
-        <img className="picture" src="AddDog.png" alt="Friendo App"/>
+        <img className="picture" src="AddDog.PNG" alt="Friendo App"/>
           <h3>Add Your Dog</h3>
           <p>Use this link to add all the information needed to find your dog their perfect match.</p>
           <Link
-            to="/signup"
+            to="/dogs/new"
             style={{
               mr: 4,
               variant: "h4",
@@ -43,15 +41,15 @@ const Home = () => {
               textAlign: "center",
             }}
           >
-            <button>Sign Up</button>
+            <button>Add Dog</button>
           </Link>
         </div>
         <div classname="side">
-        <img className="picture" src="FindFriend.png" alt="Friendo App"/>
+        <img className="picture" src="FindFriend.PNG" alt="Friendo App"/>
           <h3>Find their new friends</h3>
           <p>Search through the library of other user's dogs and start making friends.</p>
           <Link
-            to="/signup"
+            to="/dogs"
             style={{
               mr: 4,
               variant: "h4",
@@ -60,14 +58,26 @@ const Home = () => {
               textAlign: "center",
             }}
           >
-            <button>Sign Up</button>
+            <button>Dogs</button>
           </Link>
         </div>
       </div>
       <div className="learn-more-button">
-      <button >Learn more</button>
+      <Link
+            to="/learn_more"
+            style={{
+              mr: 4,
+              variant: "h4",
+              color: "black",
+              textDecoration: "none",
+              textAlign: "center",
+            }}
+          >
+            <button>Learn more</button>
+          </Link>
       </div>
-
+            <br/>
+            <br/>
     </div>
   );
 };
