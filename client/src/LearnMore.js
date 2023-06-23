7f9ecb8fb53a250fa3e857cacddc87e698b51b3f
Zachary Sirna <zsirna@yahoo.com>
2023-06-23 15:14:05 -0400

    adding a learn more component

---
+++ client/src/LearnMore.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const LearnMore = () => {
+  return (
+    <div>
+      {" "}
+      <p>
+        Anti-social dogs can not only present problems inside the house but they
+        are also more likely to be aggressive outside the house.
+      </p>
+      <h2>Issues related to anti-social behavior</h2>
+      <h3>Inside the home:</h3>
+      <ul>
+        <li>Avoids other dogs or people</li>
+        <li>Pulls or lunges on walks</li>
+        <li>Not respecting personal space</li>
+      </ul>
+      <h3>Outside the home:</h3>
+      <ul>
+        <li>Barking</li>
+        <li>Growling</li>
+        <li>Baring Teeth</li>
+      </ul>
+      <p>
+        Start here by clicking the sign up button. You can also sign up or login
+        using the user icon on the top right.
+        <br />
+        After signing up or logging in you can add a dog using the same user tab
+        in the top right.
+        <br />
+        Once signed in you can explore dogs and start creating meetups.
+      </p>
+    </div>
+  );
+};
+
+export default LearnMore;
