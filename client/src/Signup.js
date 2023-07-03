2b63154d2bb97ecf8d4eb981c4a0c641977b292b
Zachary Sirna <zsirna@yahoo.com>
2023-07-03 14:30:32 -0400

    changed the css for sign up

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,19 +1,19 @@
 import React, { useState, useContext } from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "./Context";
+import "./App.css";
 
-const Signup = ( ) => {
-
-  const {loginUser} = useContext(UserContext)
+const Signup = () => {
+  const { loginUser } = useContext(UserContext);
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [reenteredPassword, setReenteredPassword] = useState("");
   const [first_name, setFirst_name] = useState("");
-  const [county, setCounty] = useState("")
+  const [county, setCounty] = useState("");
   const [state, setState] = useState("");
   const [email, setEmail] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -28,92 +28,96 @@ const Signup = ( ) => {
         first_name,
         state,
         county,
-        email
+        email,
       }),
     }).then((r) => {
       if (r.ok) {
         r.json().then((user) => loginUser(user));
-        navigate("/")
+        navigate("/");
       }
     });
   }
-  
-  return (
-    <div>
-      <h1>Create Account</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username: </label>
-          <input 
-                    type="text"
-                    id="username"
-                    autoComplete="off"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input 
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Re-Enter Password: </label>
-          <input 
-                    type="password"
-                    id="password"
-                    value={reenteredPassword}
-                    onChange={(e) => setReenteredPassword(e.target.value)}
-                    autoComplete="current-password"
-          />
-        </div>
-        <div>
-          <label htmlFor="first_name">First Name: </label>
-          <input 
-                    type="first_name"
-                    id="first_name"
-                    value={first_name}
-                    onChange={(e) => setFirst_name(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="state">State: </label>
-          <input 
-                    type="state"
-                    id="state"
-                    value={state}
-                    onChange={(e) => setState(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="county">County: </label>
-          <input 
-                    type="county"
-                    id="county"
-                    value={county}
-                    onChange={(e) => setCounty(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email Address: </label>
-          <input 
-                    type="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        {password === reenteredPassword ? 
-        <input type="submit" value="Create Account" />
-      : <p>Passwords must match</p>}
-      </form>
-    </div>
-  )
-}
 
-export default Signup
+  return (
+    <div className="card">
+      <div className="signup-card">
+        <h1>Create Account</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              id="username"
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Re-Enter Password: </label>
+            <input
+              type="password"
+              id="reenteredPassword"
+              value={reenteredPassword}
+              onChange={(e) => setReenteredPassword(e.target.value)}
+              autoComplete="current-password"
+            />
+          </div>
+          <div>
+            <label htmlFor="first_name">First Name: </label>
+            <input
+              type="first_name"
+              id="first_name"
+              value={first_name}
+              onChange={(e) => setFirst_name(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="state">State: </label>
+            <input
+              type="state"
+              id="state"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="county">County: </label>
+            <input
+              type="county"
+              id="county"
+              value={county}
+              onChange={(e) => setCounty(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email Address: </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          {password === reenteredPassword ? (
+            <input type="submit" value="Create Account" />
+          ) : (
+            <p>Passwords must match</p>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
