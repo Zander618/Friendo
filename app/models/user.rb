9c4557ffb34f9488b58ba7ea208e2f8d5a8f54d2
Zachary Sirna <zsirna@yahoo.com>
2023-08-04 13:57:05 -0400

    adding password reset methods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,22 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :state, presence: true
   validates :county, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: {minimum: 6}, if: :password
+
+  def send_password_reset
+    self.update!(password_reset_token: self.generate_base64_token, password_reset_sent_at: Time.zone.now)
+    UserMailer.with(user: self, token: self.password_reset_token).password_reset_email.deliver_now
+  end
+
+  def reset_password(params)
+    self.update!(password: params[:password])      
+    self.update!(password_reset_token: nil, password_reset_sent_at: nil)    
+  end
+
+  private
+
+  def generate_base64_token
+    SecureRandom.urlsafe_base64
+  end
 end
