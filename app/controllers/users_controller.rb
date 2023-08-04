b0682ff853bf6204b0931b6106746c185c65d4d0
Zachary Sirna <zsirna@yahoo.com>
2023-08-04 14:00:52 -0400

    adding methods in user controller for forgot and reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,31 @@ class UsersController < ApplicationController
     render json: @current_user
   end
 
+  def forgot
+    user = User.find_by(email: params[:email])
+    if user
+      user.send_password_reset
+      render json: { alerts: ["We have sent you an email to your registered email address, please follow the instruction to reset your password." ] }, status: :created
+    else
+      render json: { errors: ["Email address not registered"] }, status: :not_found
+    end
+  end
+
+  def reset
+    user = User.find_by(email: params[:email])
+    if user
+      if user.password_reset_sent_at && user.password_reset_sent_at + 3600 >= Time.zone.now && user.password_reset_token == params[:token]
+        user.reset_password(params)
+        session[:user_id] = user.id
+        render json: {user: user, alerts: ["Your password has been reset."] }, status: :created
+      else
+        render json: { errors: ["Token has expired."] }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: ["Email address not registered."] }, status: :not_found
+    end
+  end
+
 
   private
 
