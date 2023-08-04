64570cc741546856166d6e8d57232de3404655b7
Zachary Sirna <zsirna@yahoo.com>
2023-08-04 13:52:48 -0400

    adding useContext to pass setUser to PasswordReset

--- client/src/PasswordReset.js
+++ client/src/PasswordReset.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
 import { UserContext } from "./Context";
 import { useParams, useNavigate } from "react-router-dom";
 
